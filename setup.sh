b08d7eba7308b2c7ccef8d03f27fcbc56970f0e2
zemmifobi <zemmifobi@gmail.com>
2024-09-15 13:30:58 +0200

    Update resource group and workspace names

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-#RESOURCE_GROUP_NAME="cloudml"
+#RESOURCE_GROUP_NAME="default_resource_group"
 GROUP="default_resource_group"
 LOCATION="eastus"
 WORKSPACE="test_workspace_azure_ml"
